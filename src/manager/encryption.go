481cc7d41ecf1e714af1ad6b8fa020f134412910
PlayJeri <numri.jeri@gmail.com>
2024-02-06 16:58:29 +0200

    Add encryption functionality to manager package

---
+++ src/manager/encryption.go
@@ -0,0 +1,56 @@
+package manager
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+var str = "passutaskufjdksl"
+var bytes = []byte(str)
+
+func Encrypt(plaintext []byte) []byte {
+	copy(bytes[:], str)
+	block, err := aes.NewCipher(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err)
+	}
+
+	ciphered := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphered
+}
+
+func Decrypt(ciphered []byte) []byte {
+	block, err := aes.NewCipher(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(ciphered) < gcm.NonceSize() {
+		panic("cipher text too short")
+	}
+
+	nonce, ciphered := ciphered[:gcm.NonceSize()], ciphered[gcm.NonceSize():]
+	plaintext, err := gcm.Open(nil, nonce, ciphered, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return plaintext
+}
