6a6902bb673840560e41204aa790c252864f2f10
PlayJeri <numri.jeri@gmail.com>
2024-02-06 16:58:49 +0200

    Refactor testModel.go to use proper naming conventions

--- src/manager/testModel.go
+++ src/manager/testModel.go
@@ -1,9 +1,9 @@
 package manager
 
 // New model for testing the app
-func TestModel() model {
-	return model{
-		Passwords: []passwordEntry{
+func TestModel() Model {
+	return Model{
+		Passwords: []PasswordEntry{
 			{Service: "Twitter", Username: "user1", Password: "password1"},
 			{Service: "Facebook", Username: "user2", Password: "password2"},
 			{Service: "Instagram", Username: "user3", Password: "password3"},
